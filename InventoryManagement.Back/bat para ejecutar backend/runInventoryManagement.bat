1b185f9a965b617c49f0535e79e570dab97b09c7
Matias <gramajomatias12@gmail.com>
2026-04-07 13:28:25 -0300

    feat: Add batch file to run InventoryManagement API

---
+++ InventoryManagement.Back/bat para ejecutar backend/runInventoryManagement.bat
@@ -0,0 +1,12 @@
+@echo off
+title Ejecutar InventoryManagement API
+
+echo ================================
+echo Iniciando API .NET...
+echo ================================
+
+cd /d C:\Users\Matias\source\repos\InventoryManagement\InventoryManagement.Back\InventoryManagement.API
+
+dotnet run
+
+pause
